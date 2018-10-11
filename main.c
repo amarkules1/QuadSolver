d241389c40c36e501264540f4a812c051bf52e09
JakeKampf <jacob.kampf@gmail.com>
2018-10-11 13:21:50 -0400

    This is the main method for the quad solver project, let me know if I'm stupid and I did something wrong.

---
+++ main.c
@@ -0,0 +1,17 @@
+/**
+ * Author:Jacob Kampf
+ * Purpose: Main Method for Quad Solver Program
+ * Comments: We can discuss if we need anything else for a main method
+ * Date Created: 10/11/18
+ * */
+#include <stdlib.h> 
+#include <string.h>  
+#include <stdio.h>
+
+//Main Method--------------------------------------------------------------------------------------
+int main (int argc, char const *argv[]) {
+
+
+
+	return(0);
+} 
