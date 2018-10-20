77e8f28028201f8b442eb99cf675ddd767d989ea
Alex Markules <alex.r.markules@wmich.edu>
2018-10-20 16:31:05 -0400

    added tests for quad solver, added pre-checking for determinant, and added method to determine the accuracy of solutions

--- quadSolver.c
+++ quadSolver.c
@@ -2,16 +2,41 @@
 #include <math.h>
 /*
 stores calculations in x1 and x2, 
-returns 1 for success
-(return values should change with more robust implementation)
+returns the number of real solutions to the quadratic
 */
 int quadSolver(double a, double b, double c, double * x1, double * x2){
-    double determinant = sqrt((b * b)-4*a*c);
-    double higher = (b * -1) + determinant;
-    higher = higher / (2 * a);
-    double lower = (b * -1) - determinant;
-    lower = lower / (2 * a);
-    *x1 = higher;
-    *x2 = lower;
-    return 1;
+	if(isDeterminantZero(a,b,c)){
+		*x1 = (b * (-1))/(2 * a);
+		*x2 = 0.0/0.0;
+		return 1;
+	}else if(isDeterminantNegative(a,b,c)){
+		*x1 = 0.0/0.0;
+		*x2 = 0.0/0.0;
+		return 0;
+	}else{
+		double determinant = sqrt((b * b)-4*a*c);
+    	double higher = (b * -1) + determinant;
+    	higher = higher / (2 * a);
+    	double lower = (b * -1) - determinant;
+    	lower = lower / (2 * a);
+    	*x1 = higher;
+    	*x2 = lower;
+    	return 2;
+	}
+}
+
+//returns 1 if the determinant is within 0.0000001 of zero, else returns 0
+int isDeterminantZero(double a, double b, double c){
+	double determinant = (b * b) - (4 * a * c);
+	return approxZero(determinant);
+}
+
+//returns 1 if the determinant is negative, else 0
+int isDeterminantNegative(double a, double b, double c){
+	double determinant = (b * b) - (4 * a * c);
+	return (determinant < 0) ? 1 : 0;
+}
+//returns 1 if z is within 0.0000001 of zero, els returns 0
+int approxZero(double z){
+	return (z < 0.0000001 && z > -0.0000001)? 1 : 0;
 }
--- quadSolver.h
+++ quadSolver.h
@@ -3,4 +3,10 @@
 
 int quadSolver(double a, double b, double c, double * x1, double * x2);
 
+int isDeterminantZero(double a, double b, double c);
+
+int isDeterminantNegative(double a, double b, double c);
+
+int approxZero(double z);
+
 #endif
