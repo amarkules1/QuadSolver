0949fdeb979560ed81b0a979188c751e470cc8de
Alex Markules <alex.r.markules@wmich.edu>
2018-11-29 14:40:32 -0500

    aa

--- answerChecker.c
+++ answerChecker.c
@@ -1,11 +1,12 @@
 #include "answerChecker.h"
 
-//returns 0 if x is accurate, -1 if 0.99x is more accurate, and 1 if 1.01x is more accurate
+//returns 0 if x is accurate, -1 if 0.99999x is more accurate, and 1 if 1.00001x is more accurate to indicate that the solution may be inaccurate
+
 int checkAnswer(double a, double b, double c, double x){
 	double val = fabs((a * x * x) + (b * x) + c);
-	double xSmall = 0.99 * x;
+	double xSmall = 0.99999 * x;
 	double valSmall = fabs((a * xSmall * xSmall) + (b * xSmall) + c);
-	double xBig = 1.01 * x;
+	double xBig = 1.00001 * x;
 	double valBig = fabs((a * xBig * xBig) + (b * xBig) + c);
 
 	if(val <= valSmall && val <= valBig){
